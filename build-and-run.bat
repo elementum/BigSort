8bd9f1bd40f6640b6ea38f718c6da2dcf125ad3b
Eugene Doronin <e.doronin@velvetech.ru>
2021-03-07 12:26:00 +0700

    fix missing generating file in bat file

--- build-and-run.bat
+++ build-and-run.bat
@@ -1,3 +1,3 @@
 dotnet build -c Release
-
+"FileGenerator/bin/Release/net5.0/FileGenerator.exe" --size 10 --outFile "%cd%\file_to_sort.txt"
 "FileSorter/bin/Release/net5.0/FileSorter.exe" --inFile "%cd%\file_to_sort.txt" --outFile "%cd%\sorted.txt"
